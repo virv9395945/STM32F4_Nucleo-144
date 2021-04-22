dad693491bbbcc099247b02e4d9cee2d7833b193
virv9395945 <virv9395945@gmail.com>
2021-04-22 15:24:04 -0400

    update

---
+++ i2c/lcd1602 i2c/Core/Inc/device.h
@@ -0,0 +1,41 @@
+
+/********************************************************************************
+ * Project Name			 :   <<<<  Nombre del Proyecto  >>>>
+ * File Name				 : main.c
+ * Revision				 	 :	ver 1.0 (marzo-2020)
+ * Microcontroller name  : STM32F4xx ARM
+ * Compiler				 	 : Keil MDK_ARM 4-5
+ * CubeMX					 : STM32CubeMX - version 5.4.0
+ * Firmware CubeMX		 : STM32Cube FW_F4 v1.24.2
+ * Author					 : Victor Rodriguez V.
+ * E-mail					 : virv9395945@gmail.com
+ * Date 					 	 : 10.03.2020		<<< Poner fecha creacion >>>
+ * Modify					 : 10.03.2020
+ * Important				 : <<<< comentario corto >>>>
+ * Path	project 			 : ...\Desktop\Stmicrolectronics\Stm32f407\20 - Plantilla nokia 5110
+ * Path library			 : ...\Desktop\Stmicrolectronics\Library\Inc
+ *								   ...\Desktop\Stmicrolectronics\Library\Scr
+ * =============================================================================
+ * 
+ * << Detallar lo mas amplio y directo posible, la funcionalidad del programa >> 
+ * 
+ *   C:\Users\Alexis\Desktop\Stmicrolectronics\Stm32f407\00 - Plantillas\main.c
+ * 
+ * 
+ * @file           : main.c
+ * @brief          : Main program body
+ *
+ ******************************************************************************/
+
+
+/* Includes ------------------------------------------------------------------*/
+
+// MCU Stm32F407vg - 168MHz
+//#include "stm32f4xx_hal.h"
+
+
+// MCU Stm32F103C8 - 72MHz
+//#include "stm32f1xx_hal.h"
+
+// MCU Stm32F103C8 - 168MHz
+#include "stm32f4xx_hal.h"
---
+++ i2c/lcd1602 i2c/Core/Src/i2c-lcd.c
@@ -0,0 +1,147 @@
+
+/** Put this in the src folder **/
+
+#include "i2c-lcd.h"
+#include "stdio.h"
+
+extern I2C_HandleTypeDef I2C_LCD; 			 // change your handler here accordingly
+
+//extern   char Linea_1[20];
+//extern   char Linea_2[20];
+
+extern     char 		display[2][16];
+
+/**
+  ******************************************************************************
+  * @brief	Direccion I2C del dispositivo, se entrega valor desplazado y unico
+  *			para lectura y escritura
+  ******************************************************************************
+  */
+#define SLAVE_ADDRESS_LCD 0x27 << 1
+
+
+/**
+  ******************************************************************************
+  * @brief	Declaraci�n variables denerales
+  ******************************************************************************
+  */
+ #define timeout 100 
+  
+/**
+  ******************************************************************************
+  * @brief	go
+  * @param	
+  * @retval	
+  ******************************************************************************
+  */
+void lcd_send_cmd (char cmd)
+{
+   char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (cmd&0xf0);
+	data_l = ((cmd<<4)&0xf0);
+	data_t[0] = data_u|0x0C;  //en=1, rs=0
+	data_t[1] = data_u|0x08;  //en=0, rs=0
+	data_t[2] = data_l|0x0C;  //en=1, rs=0
+	data_t[3] = data_l|0x08;  //en=0, rs=0
+	HAL_I2C_Master_Transmit (&I2C_LCD, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, timeout );
+}
+/**
+  ******************************************************************************
+  * @brief	
+  * @param	
+  * @retval	
+  ******************************************************************************
+  */
+void lcd_send_data (char data)
+{
+	char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = (data&0xf0);
+	data_l = ((data<<4)&0xf0);
+	data_t[0] = data_u|0x0D;  //en=1, rs=0    1101
+	data_t[1] = data_u|0x09;  //en=0, rs=0    1001
+	data_t[2] = data_l|0x0D;  //en=1, rs=0    1101
+	data_t[3] = data_l|0x09;  //en=0, rs=0    1001
+	HAL_I2C_Master_Transmit (&I2C_LCD, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, timeout );	
+}
+/**
+  ******************************************************************************
+  * @brief	
+  * @param	
+  * @retval	
+  ******************************************************************************
+  */
+void lcd_init (void)
+{
+	lcd_send_cmd (0x01);
+	HAL_Delay(4);
+	lcd_send_cmd (0x02);
+	HAL_Delay(4);
+	lcd_send_cmd (0x28);
+	HAL_Delay(4);
+	
+	lcd_send_cmd (0x0c);
+	HAL_Delay(4);
+	lcd_send_cmd (0x80);
+	HAL_Delay(4);
+}
+/**
+  ******************************************************************************
+  * @brief	
+  * @param	
+  * @retval	
+  ******************************************************************************
+  */
+void lcd_send_string (char *str)
+{
+	while (*str) lcd_send_data (*str++);
+}
+/**
+  ******************************************************************************
+  * @brief	
+  * @param	
+  * @retval	
+  ******************************************************************************
+  */
+void lcd_send_string_file (uint8_t File, char *str)
+{
+	if ( File == 0 )
+		lcd_send_cmd (0x80);
+	else
+		lcd_send_cmd (0xc0);
+
+	lcd_send_string (str);
+}
+/**
+  ******************************************************************************
+  * @brief	
+  * @param	
+  * @retval	
+  ******************************************************************************
+  */
+void lcd_send_string_cr (char *str)
+{
+	sprintf(display[0],"%s", display[1]);
+	sprintf(display[1],"%s", str);
+
+	lcd_send_string_file (File_1, display[0]);
+	lcd_send_string_file (File_2, display[1]);		
+}
+
+/*
+ * @brief Print a string on the LCD
+ * @param x: starting point on the x-axis (column)
+ * @param y: starting point on the y-axis (line)
+ */
+void LCD_print(char *str, uint8_t x, uint8_t y){
+	
+	if ( y == 0 )
+		lcd_send_cmd (0x80 );
+	else
+		lcd_send_cmd (0xc0 );
+	
+	lcd_send_string_file ( y , str);
+	
+}
+
